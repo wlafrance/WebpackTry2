3c3cbcc4195e2e5709c1176580c19e7aca32e369
Warren LaFrance <warrenlafrance@outlook.com>
2018-03-20 15:05:08 -0500

    init
    `init

---
+++ WebpackTry2/Scripts/myviewmodel.ts
@@ -0,0 +1,26 @@
+﻿import * as ko from "knockout";
+import * as $ from "jquery";
+import * as d3 from 'd3';
+import * as jsPDF from 'jspdf';
+
+
+class MyViewModel {
+    firstname: KnockoutObservable<string>;
+    lastname: KnockoutObservable<string>;
+
+    constructor(firstname: string, lastname: string) {
+        this.firstname = ko.observable(firstname);
+        this.lastname = ko.observable(lastname);
+       
+    }
+}
+
+var jQueryItem = $('#jqueryTest').css("background", "red");
+ko.applyBindings(new MyViewModel("Jakob", "Christensen"));
+
+const square = d3.selectAll("rect");
+square.style("fill", "orange");
+
+// Default export is a4 paper, portrait, using milimeters for units
+//var doc = new jsPDF();
+//debugger;
---
+++ WebpackTry2/webpack.config.js
@@ -0,0 +1,25 @@
+﻿var path = require('path');
+
+module.exports = {
+    entry: {
+        site: [
+            './wwwroot/js/site.js',
+            './scripts/myviewmodel.ts']
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'wwwroot/dist/')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ]
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"]
+    }
+};
